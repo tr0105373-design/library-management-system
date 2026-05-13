fba6f6ece1f65ba2b7ba1847b4e6d9a970e3f177
Taniya Rajput <tr0105373@gmail.com>
2026-05-13 17:42:05 +0530

    fix mysql duplicate

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,32 +3,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// const db = mysql.createConnection({
-//   host: mysql.railway.internal,
-//   user:root ,
-//   password: ePlxPYVuDivhunrdEeJWcIgmedarftom,
-//   database: "railway"
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error('Database connection failed:', err);
-//     return;
-//   }
-//   console.log('MySQL Database Connected!');
-// });
-
-// module.exports = db;
-
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-// });
-
-const mysql = require("mysql2");
-
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
