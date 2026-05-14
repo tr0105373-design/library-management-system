f8b00904b3e8c511fb0af5329119446b06237f3d
Taniya Rajput <tr0105373@gmail.com>
2026-05-14 10:59:34 +0530

    fix express route error

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,12 @@ app.use('/api/reports', reportRoutes);
 // Serve frontend
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
