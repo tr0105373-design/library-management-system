a9d0202a501e9633121fbc105a20f42854a960e9
Taniya Rajput <tr0105373@gmail.com>
2026-05-14 10:19:15 +0530

    convert mysql to postgres

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,45 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+// const mysql = require('mysql2');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+// const db = mysql.createConnection({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   port: process.env.MYSQLPORT
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log("Database connection failed:", err);
+//     return;
+//   }
+//   console.log("MySQL connected");
+// });
+
+// module.exports = db;
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-db.connect((err) => {
+pool.connect((err) => {
   if (err) {
     console.log("Database connection failed:", err);
-    return;
+  } else {
+    console.log("PostgreSQL connected");
   }
-  console.log("MySQL connected");
 });
 
-module.exports = db;
+module.exports = pool;
