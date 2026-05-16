00a9d88742e703b1ce4c7e9d6c52bd8f8875fd77
Taniya Rajput <tr0105373@gmail.com>
2026-05-16 18:59:48 +0530

    clean build fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,7 +5,6 @@ const api = axios.create({
   baseURL: API_URL,
 });
 
-// token auto attach (IMPORTANT for 401 fix)
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
