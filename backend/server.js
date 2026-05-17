6e67ed3654b7ad30d1dfe834f6643ba5e7ab8111
Taniya Rajput <tr0105373@gmail.com>
2026-05-17 22:25:24 +0530

    fix express wildcard route

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,42 @@
+// const express = require('express');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
+// const path = require("path");
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// /* -------- API ROUTES -------- */
+
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/books", require("./routes/bookRoutes"));
+// app.use("/api/members", require("./routes/memberRoutes"));
+// app.use("/api/issues", require("./routes/issueRoutes"));
+// app.use("/api/fines", require("./routes/fineRoutes"));
+// app.use("/api/reports", require("./routes/reportRoutes"));
+
+// /* -------- FRONTEND -------- */
+
+// const frontendPath = path.join(__dirname, "../frontend/dist");
+
+// app.use(express.static(frontendPath));
+
+// /* React routing fallback */
+// app.use((req, res) => {
+//   res.sendFile(path.join(frontendPath, "index.html"));
+// });
+
+// /* -------- SERVER -------- */
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log("Server running on port", PORT);
+// });
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -10,7 +49,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* -------- API ROUTES -------- */
+/* ---------------- API ROUTES ---------------- */
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/books", require("./routes/bookRoutes"));
@@ -19,21 +58,20 @@ app.use("/api/issues", require("./routes/issueRoutes"));
 app.use("/api/fines", require("./routes/fineRoutes"));
 app.use("/api/reports", require("./routes/reportRoutes"));
 
-/* -------- FRONTEND -------- */
+/* ---------------- FRONTEND ---------------- */
 
-const frontendPath = path.join(__dirname, "../frontend/dist");
+const frontendPath = path.join(__dirname, '../frontend/dist');
 
 app.use(express.static(frontendPath));
 
-/* React routing fallback */
-app.use((req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-/* -------- SERVER -------- */
+/* ---------------- SERVER ---------------- */
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
