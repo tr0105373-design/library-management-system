6139fa359b8d411e35234c0063d561db2afb335f
Taniya Rajput <tr0105373@gmail.com>
2026-05-17 22:33:20 +0530

    fix express wildcard route

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* ---------------- API ROUTES ---------------- */
+/* API ROUTES */
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/books", require("./routes/bookRoutes"));
@@ -19,18 +19,18 @@ app.use("/api/issues", require("./routes/issueRoutes"));
 app.use("/api/fines", require("./routes/fineRoutes"));
 app.use("/api/reports", require("./routes/reportRoutes"));
 
-/* ---------------- FRONTEND ---------------- */
+/* FRONTEND */
 
 const frontendPath = path.join(__dirname, "../frontend/dist");
 
 app.use(express.static(frontendPath));
 
 /* React Router fallback */
-app.get("/:path(*)", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-/* ---------------- SERVER ---------------- */
+/* SERVER */
 
 const PORT = process.env.PORT || 5000;
 
