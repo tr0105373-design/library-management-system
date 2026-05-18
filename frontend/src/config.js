e1fc44eac4e9790d581205a5db35142b5a31b10b
Taniya Rajput <tr0105373@gmail.com>
2026-05-18 09:24:56 +0530

    add config api url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://library-management-system-z8fc.onrender.com";
+// export const API_URL = "https://library-management-system-z8fc.onrender.com";
+export const API_URL = import.meta.env.VITE_API_URL;
