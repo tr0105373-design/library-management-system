756daff49b49ae3442ae45b21a6873b5581b2f38
Taniya Rajput <tr0105373@gmail.com>
2026-05-18 11:36:44 +0530

    fix api base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://library-management-system-z8fc.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
